3c364cd35b04282e0730af4138eeba046781671a
Mauricio <Ramos@Mauricios-MBP.home>
2016-04-09 21:48:25 -0400

    revision hw

--- key.js
+++
@@ -1,9 +0,0 @@
-
- 
-exports.twitterKeys ={
-  consumer_key: 't7SiYaQi1mVERcsns19bNWxXr',
-  consumer_secret: 'f2dz659GWKAGMOukAcYZSdkkCV9temnkCwzOEU8aUhpsbVaGIF',
-  access_token_key: '715349953841864704-7tBEdRMVSTqmYQKn40kB8WDQdJJRfSN',
-  access_token_secret: 'TYcH150lPVFYVG027wbm1sypNkRA9uyE7sctoP8oX7bDU'
-}
- 
